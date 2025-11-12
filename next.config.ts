d1beb3753eb934e67c9503cc10afb26dacd9081a
Sarthak Goyal <sarthakgoyak487@gmail.com>
2025-11-12 21:28:31 +0530

    fix: change in base path in config

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/" : "",
-  assetPrefix: isProd ? "/" : "",
+  basePath: "",
+  assetPrefix: "",
   images: {
     unoptimized: true,
   },
